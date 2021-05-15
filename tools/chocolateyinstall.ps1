a176043c8c06186d1630cd88565523837ea9d1c6
Bob Tanner <tanner@real-time.com>
2021-05-14 19:52:42 -0500

    WIP: #6 Looks like interopformsredist is part of Visual Studio
      * Move .msi to private repository

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,28 +1,17 @@
-﻿$ErrorActionPreference = 'Stop'; 
+﻿$ErrorActionPreference = 'Stop';
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.microsoft.com/download/3/7/D/37D90824-1BE8-4085-920E-54812C045A55/microsoft.interopformsredist.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'MSI'
-  url           = $url
+  fileType      = 'msi'
+  silentArgs    = "/qn /norestart"
+  url           = 'http://gold-images.int.celadonsystems.com/SYSPRO/SYSPRO7_Update1_2016 - Lucy/SYSPRO/ISSetupPrerequisites/{58E08727-9947-4FC4-BA00-9C3B80A5DAF6}/microsoft.interopformsredist.msi'
+  softwareName  = 'interopformsredist*'
 
-  softwareName  = 'interopformsredist*' 
-
-  # Checksums are now required as of 0.10.0.
-  #
   checksum      = 'a1f643c82c1be0ecaad920e5c1c11bb0006403788aa733212a6359d76f850399'
-  checksumType  = 'sha256' 
-
-
-  # Cygwin $env:TEMP is C:\tools\cygwin\tmp\chocolatey
-  # Powershell $env:TEMP is C:\Users\tanner\AppData\Local\Temp
-  #
-  #
-  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).log`""
-  validExitCodes= @(0, 3010, 1641)
+  checksumType  = 'sha256'
 }
 
-Install-ChocolateyPackage @packageArgs 
+Install-ChocolateyPackage @packageArgs
