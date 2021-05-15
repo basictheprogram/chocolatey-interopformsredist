bf2e0bff41e53b1f400f1bb7dde3feb04a7c0cd8
Bob Tanner <tanner@real-time.com>
2021-05-14 20:45:28 -0500

    Update chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   fileType      = 'msi'
   silentArgs    = "/qn /norestart"
-  url           = 'http://gold-images.int.celadonsystems.com/SYSPRO/SYSPRO7_Update1_2016 - Lucy/SYSPRO/ISSetupPrerequisites/{58E08727-9947-4FC4-BA00-9C3B80A5DAF6}/microsoft.interopformsredist.msi'
+  url           = ''
   softwareName  = 'interopformsredist*'
 
   checksum      = 'a1f643c82c1be0ecaad920e5c1c11bb0006403788aa733212a6359d76f850399'
