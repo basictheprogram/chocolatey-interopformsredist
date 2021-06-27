f9787ccfbbfee218af9ca58427de5dc14aef0515
Bob Tanner <tanner@real-time.com>
2021-06-26 21:42:31 -0500

    WIP: https://support.real-time.com/support/index.html?q=77151

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,17 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$data = & (Join-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Path) -ChildPath data.ps1)
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'msi'
-  silentArgs    = "/qn /norestart"
-  url           = 'http://gold-images.int.celadonsystems.com/SYSPRO/SYSPRO7_Update1_2016 - Lucy/SYSPRO/ISSetupPrerequisites/{58E08727-9947-4FC4-BA00-9C3B80A5DAF6}/microsoft.interopformsredist.msi'
-  softwareName  = 'interopformsredist*'
+    packageName   = $env:ChocolateyPackageName
+    unzipLocation = $toolsDir
+    fileType      = 'msi'
+    silentArgs    = "/qn /norestart"
+    softwareName  = 'interopformsredist'
 
-  checksum      = 'a1f643c82c1be0ecaad920e5c1c11bb0006403788aa733212a6359d76f850399'
-  checksumType  = 'sha256'
+    url           = $data.url
+    checksum      = $data.checksum
+    checksumType  = $data.checksumType
 }
 
 Install-ChocolateyPackage @packageArgs
